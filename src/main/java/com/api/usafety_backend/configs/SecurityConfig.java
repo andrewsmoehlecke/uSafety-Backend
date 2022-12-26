877dc212794bacd7fffc45955bfbe0e958dd7ffe
oAngulo-sh <andrewsmoehlecke@gmail.com>
2022-12-26 14:32:54 -0300

    Ajustando configuração do CORS

--- src/main/java/com/api/usafety_backend/configs/SecurityConfig.java
+++ src/main/java/com/api/usafety_backend/configs/SecurityConfig.java
@@ -48,11 +48,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
+                .cors().disable()
+                .csrf().disable()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
-                .addFilterBefore(authenticationTokenFilterBean(), UsernamePasswordAuthenticationFilter.class)
-                .cors().disable()
-                .csrf().disable();
+                .addFilterBefore(authenticationTokenFilterBean(), UsernamePasswordAuthenticationFilter.class);
 
         http.authorizeRequests()
                 .antMatchers(HttpMethod.OPTIONS, "/**").permitAll()
